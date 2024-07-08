a1985226b9c6757d4fef24edffd2489f9a8fcb6b
Marcela Silva <110864304+Marcela-SB@users.noreply.github.com>
2024-07-08 14:01:11 -0300

    Update funcGeren.py

--- funcoes/funcGeren.py
+++ funcoes/funcGeren.py
@@ -1,5 +1,6 @@
 from modulos import *
 from conexaoBD import *
+from funcoes.bdBuscar import *
 
 tk.botao = ""
 
@@ -7,31 +8,37 @@ def funcBtExcluir(self):
     opcaoExluir = messagebox.askyesno("Excluir item", "Confirma a exclusão do item?")
 
     if(opcaoExluir):
-        #CONECTANDO O BD E INICIALIZANDO CURSOR
-        conexao = conectar_bd(self)
-        cursor = conexao.cursor()
-
         # RECEBENDO OS VALORES DOS ENTRYS
         excTombo = str(self.inputTomboGer.get())
 
         # SE O VALOR NÃO FOR NULL
         if(excTombo):
-            try:
-                cursor.execute("DELETE FROM item WHERE tombo = %s", (excTombo,))
-                conexao.commit()
-                messagebox.showinfo("Sucesso", "Item excluído com sucesso!")
-        
-                # APAGANDO DOS INPUTS
-                self.inputTomboGer.delete(0,END)
-                self.inputItemGer.delete(0, END)
-                self.inputDescricaoGer.delete(0,END)
-                self.valor_combobox.set("")
 
-            except mysql.connector.Error as err:
-                messagebox.showerror("Erro", f"Erro ao excluir item: {err}")
-            finally:
-                cursor.close()
-                conexao.close()
+            # Verificando se o item existe antes de excluir
+            if verificarItem(self, excTombo):
+                
+                #CONECTANDO O BD E INICIALIZANDO CURSOR
+                conexao = conectar_bd(self)
+                cursor = conexao.cursor()
+                try:
+                    cursor.execute("DELETE FROM item WHERE tombo = %s", (excTombo,))
+                    conexao.commit()
+                    messagebox.showinfo("Sucesso", "Item excluído com sucesso!")
+            
+                    # APAGANDO DOS INPUTS
+                    self.inputTomboGer.delete(0,END)
+                    self.inputItemGer.delete(0, END)
+                    self.inputDescricaoGer.delete(0,END)
+                    self.valor_combobox.set("")
+
+                except mysql.connector.Error as err:
+                    messagebox.showerror("Erro", f"Erro ao excluir item: {err}")
+                finally:
+                    cursor.close()
+                    conexao.close()
+
+            else:
+                messagebox.showwarning("Aviso", f"Item com tombo {excTombo} não encontrado. Não foi possível excluir.")
 
         else:
             messagebox.showerror("ERRO", "Campo de TOMBO em branco, por favor preencher!")
@@ -51,6 +58,11 @@ def funcBtAdicionar(self):
 
     # SE TODOS OS VALORES NÃO FOREM NULL
     if(adicTombo and adicItem and adicDescricao and adicSala):
+        
+        # Verifica se o item já existe
+        if verificarItem(self, adicTombo):
+            if not messagebox.askyesno("Item existente", "Item já existe, deseja sobrescrever?"):
+                return  # Cancela a adição se o usuário não deseja sobrescrever
         try:
             cursor.execute("INSERT INTO item (tombo, tipo, ident, salaId) VALUES (%s, %s, %s, %s)", (adicTombo, adicItem, adicDescricao, adicSala))
             conexao.commit()
