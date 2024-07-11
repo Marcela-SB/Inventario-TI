06c80630f71bae8417cd8a4a05863a3bc308b090
Marcela Silva <110864304+Marcela-SB@users.noreply.github.com>
2024-07-11 13:52:35 -0300

    Update funcNovoHist.py

--- funcoes/funcNovoHist.py
+++ funcoes/funcNovoHist.py
@@ -1,9 +1,42 @@
 from modulos import *
+from conexaoBD import *
 
 tk.botao = ""
 
 def funcBtNovoHist(self):
     opcaoNovoHist = messagebox.askyesno("Novo histórico", "Confirma a criação da nova Entrada no Histórico?\nVerifique as informações com cuidado.")
+
+    #RECEBENDO DADOS
+    nvhTombo = self.inputTomboNovHist.get()
+    nvhOrigem = self.selOrigem.get()
+    nvhDestino = self.selDestino.get()
+    nvhResp = self.selResponsavel.get()
+
+    if(nvhTombo and nvhOrigem and nvhDestino and nvhResp):
+        ''try:
+        
+            # Conectar ao banco de dados
+            conexao = conectar_bd(self)
+            cursor = conexao.cursor()
+            
+            '''cursor.execute("INSERT INTO item (tombo, tipo, ident, salaId) VALUES (%s, %s, %s, %s)", (adicTombo, adicItem, adicDescricao, adicSala))
+            conexao.commit()
+            messagebox.showinfo("Sucesso", "Item adicionado com sucesso!")
+    
+            # APAGANDO DOS INPUTS
+            self.inputTomboGer.delete(0,END)
+            self.inputItemGer.delete(0, END)
+            self.inputDescricaoGer.delete(0,END)
+            self.valor_combobox.set("")
+
+        except mysql.connector.Error as err:
+            messagebox.showerror("Erro", f"Erro ao adicionar item: {err}")'''''
+        finally:
+            cursor.close()
+            conexao.close()
+    else:
+        messagebox.showerror("ERRO", "Campos em branco, por favor preencher todos!")
+
     print(opcaoNovoHist)
     if(opcaoNovoHist):
         tk.botao = "NV"
