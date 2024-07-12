ce07adc3497d8696a9a8693f04594b9b20707cab
Marcela Silva <110864304+Marcela-SB@users.noreply.github.com>
2024-07-12 11:18:59 -0300

    Create criardb.py

---
+++ criardb.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+#Cria o database dentro do mySQL é executado se não houver a existência do database Inventario *não editar o arquivo
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="admin1234",
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("DROP DATABASE inventario")
+
+mycursor.execute("CREATE DATABASE inventario")
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="admin",
+    password="admin1234",
+    database="inventario"
+)
+mycursor = mydb.cursor()
+
+# criando tabela salas
+mycursor.execute("CREATE TABLE salas (salaId CHAR(3) PRIMARY KEY, tipo BINARY(0), descricao VARCHAR(30), predio VARCHAR(10))")
+
+# criando tabela item
+mycursor.execute("CREATE TABLE item (tombo CHAR(10) PRIMARY KEY, tipo VARCHAR(20), ident VARCHAR(50), salaId CHAR(3), FOREIGN KEY (salaId) REFERENCES salas(salaId))")
+
+# criando tabela movimentação
+mycursor.execute("CREATE TABLE MOVIMENTACAO (id INT AUTO_INCREMENT PRIMARY KEY, ItemID CHAR(10), FOREIGN KEY(ItemID) REFERENCES item(tombo), salaOrigemID CHAR(3), FOREIGN KEY(salaOrigemID) REFERENCES salas(salaId), salaDestinoID CHAR(3), FOREIGN KEY(salaDestinoID) REFERENCES salas(salaId), data DATE)")
+
+
