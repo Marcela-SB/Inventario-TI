01475d55b2007a2bb91affbf31fbf7f2161fd99f
Vinicius <vbarbosa.mg@gmail.com>
2024-07-19 14:57:54 -0300

    att buscarMov usando LIKE
    Ainda falta completar, contém alguns erros

--- funcoes/funcMov.py
+++ funcoes/funcMov.py
@@ -11,29 +11,44 @@ def funcbtBuscarMov(self):
 
     # RECEBENDO OS VALORES DOS ENTRYS
     tbMov = self.inputTomboMov.get()
+    dataMov = self.inputDataMov.get()
+    salaMov = self.comboboxMov.get()
 
     # SE TODOS OS VALORES NÃO FOREM NULL
-    if tbMov:
+    if tbMov or dataMov or salaMov:
+        # RESTRIÇÕES DE BUSCA
+        restricoes = []
+        parametros = []
+
+        if tbMov:
+            restricoes.append("tombo LIKE %s")
+            parametros.append(f"{tbMov}%")
+        
+        if dataMov:
+            restricoes.append("tipo WHERE %s")
+            parametros.append(f"{dataMov}")
+        
+        if salaMov:
+            restricoes.append("salaId = %s")
+            parametros.append(salaMov)
+
+        restricoes_sql = " AND ".join(restricoes)
+
         try:
-            cursor.execute(f"""
-                SELECT m.data, m.salaOrigem, m.salaDestino, m.responsavel, i.descricao 
-                FROM movimentacao m
-                JOIN item i ON m.itemID = i.tombo 
-                WHERE i.tombo = {tbMov}
-                ORDER BY m.id ASC
-            """)
+            query = f"""SELECT tombo, tipo, descricao, salaId, obs FROM item WHERE {restricoes_sql} 
+            ORDER BY tombo ASC"""
+            cursor.execute(query, parametros)
             resultados = cursor.fetchall()  # Ler todos os resultados
 
             # Limpar Treeview
-            self.entradas.delete(*self.entradas.get_children())
+            self.lista.delete(*self.lista.get_children())
 
             # Exibir resultados
             for idx, resultado in enumerate(resultados, start=1):
-                data, salaOrigem, salaDestino, responsavel, item = resultado
-                self.entradas.insert("", END, iid=idx, text=idx, values=(tbMov, item, salaOrigem, salaDestino, data, "?? : ??", responsavel))
+                tombo, item, salaOrigem, salaDestino, responsavel = resultado
+                self.entradas.insert("", END, iid=idx, text=idx, values=(tombo, item, salaOrigem, salaDestino,"?? : ??", "hora", responsavel))
 
             conexao.commit()
-
         except mysql.connector.Error as err:
             messagebox.showerror("Erro", f"Erro ao buscar item: {err}")
         finally:
